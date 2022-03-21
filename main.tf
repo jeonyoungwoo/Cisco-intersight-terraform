79ee9dd42dbebfe44a80ab3145d251480d21d915
jeonyoungwoo <26229501+jeonyoungwoo@users.noreply.github.com>
2022-03-22 01:00:45 +0900

    main

---
+++ main.tf
@@ -0,0 +1,6 @@
+/*
+Terraform Configuration Repository to deploy Converged Infrastructure stack in Intersight Managed Mode (IMM)
+There is an order of operation to be followed.  Hence we have mutliple directories created for such operations.
+
+Hence we don't define anything in the root module. Please follow the README to deploy solution.
+*/
